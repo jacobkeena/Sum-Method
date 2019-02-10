edee3cd6139e1d8e3a67ad6600dcf5c37fdec027
Jake Keena <46759124+jacobkeena@users.noreply.github.com>
2019-02-10 16:21:52 -0500

    Add files via upload

---
+++ Simple Sum Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Sum_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = Sum(x, y);
+            Console.WriteLine(z);
+            Console.Read();
+        }
+          static int Sum(int x, int y)
+            {
+                return x + y;
+            }
+    }
+}
